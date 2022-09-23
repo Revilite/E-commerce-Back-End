3943eaad573c54247b7058c1e05f0d05ab422e8d
Jacob Lockhart <procoder165@gmail.com>
2022-09-23 15:45:11 -0400

    added created messaGES

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,7 +42,7 @@ router.post('/', async (req, res) => {
   // create a new category
   try{
     const data = await Category.create(req.body)
-    res.status(200).json(data);
+    res.status(200).json({message: "Category Created!"});
   }
   catch (err){
     res.status(500).json(err);
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,7 +41,7 @@ router.post('/', async (req, res) => {
   // create a new tag
   try{
     const data = await Tag.create(req.body);
-    res.status(200).json(data);
+    res.status(200).json({message: "Tag Created!"});
   }
   catch (err) {
     res.status(500).json(err);
@@ -57,7 +57,7 @@ router.put('/:id', async (req, res) => {
         id: req.params.id,
       }
     });
-    res.status(200).json({message:"Category Updated"})
+    res.status(200).json({message:"Tag Updated!"})
   }
   catch(err){
     res.status(500).json(err);
@@ -73,7 +73,7 @@ router.delete('/:id', async (req, res) => {
         id: req.params.id,
       }
     });
-    res.status(200).json({message: "Tag Deleted"});
+    res.status(200).json({message: "Tag Deleted!"});
   }
   catch (err){
     res.status(500).json(err);
